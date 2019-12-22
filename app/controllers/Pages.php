d2cb5fb45dee99211096fd2610221cba7a14483d
Khoi Hoang <hello@koicha.dev>
2019-12-22 06:57:04 +0100

    Added Mapping Methods & Parameters
    Added Mapping Methods & Parameters for the core.php. 
    
    We want to use this in order to access method from our url. Example would be /mvc/about/33.
    
    The 33 number is the parameter of the about() function/method for the URL

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,17 @@
 <?php
   class Pages {
     public function __construct(){
-      echo 'Pages loaded';
+    }
+
+    // We need to have an index method, otherwise we get an error. The index is the default webpage when we visit it
+    public function index() {
+
+    }
+
+    public function about($id) {
+
+      echo $id;
     }
   }
+
+?>
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -32,6 +32,23 @@
 
       // Instantiate controller class
       $this->currentController = new $this->currentController;
+
+      // Check for second part of URL
+      if(isset($url[1])) {
+        // Check to see if method exists in controller 
+        if(method_exists($this -> currentController, $url[1])) {
+          $this -> currentMethod = $url[1];
+          // Unset 1 index in order if we want to use e.g. about/whatever_in_here
+          unset($url[1]);
+        }
+      }
+
+      // Get paramts
+      $this -> params = $url ? array_values($url) : [];
+
+      // Call a callback with array of params
+      call_user_func_array([$this -> currentController, $this -> currentMethod], $this -> params);
+
     }
 
     public function getUrl(){
